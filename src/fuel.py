b0c211572a1912409ebd489865afb2b3b3d407d4
Benjamin-ESME <benjamin.ducourtioux789@gmail.com>
2026-04-11 19:01:42 +0000

    update

--- src/fuel.py
+++ src/fuel.py
@@ -12,7 +12,7 @@ def fuel(nb_avion):                       # fonction pour tester "perte_fuel"
 
 def perte_fuel(dico):                    # permet de faire baisser de 0.2 le carburant de tout les avions
     for i in dico:                       # Le carburant baisse de 1 à chaque fois qu'un avion se pose (toute les 5min)
-        i["fuel"] -= 1
+        i["fuel"] -= 0.2
     return dico
 
 def fuel_avions(liste):                   # permet de supprimer les avions sans carburant 
